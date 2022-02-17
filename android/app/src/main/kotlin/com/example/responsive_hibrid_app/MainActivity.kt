939a4328336870600360db1a9be90be49dd3841b
Mateus Lopes <mateusdevelopes@gmail.com>
2022-02-17 01:12:35 -0300

    Init

---
+++ android/app/src/main/kotlin/com/example/responsive_hibrid_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.responsive_hibrid_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_hibrid_app/responsive/desktop_body.dart';
+import 'package:responsive_hibrid_app/responsive/mobile_body.dart';
+import 'package:responsive_hibrid_app/responsive/responsive_layout.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: ResponsiveLayout(
+        mobileBody: MobileBody(),
+        desktopBody: DesktopBody(),
+      ),
+    );
+  }
+}
